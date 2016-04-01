38bd58ccecc2ff21c6a77ca6a4473fac71041ca2
lerchunt <lerchunt@f216-06.e-im2ag.ujf-grenoble.fr>
2016-04-01 10:47:29 +0200

    ajouts des dossiers de base

---
+++ projet/src/main/java/fr/uga/erods/App.java
@@ -0,0 +1,13 @@
+package fr.uga.erods;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
