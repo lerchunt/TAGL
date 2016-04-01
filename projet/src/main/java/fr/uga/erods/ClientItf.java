470facb27f35e7e56cd3f16febb710b25517ed70
lerchunt <lerchunt@f216-06.e-im2ag.ujf-grenoble.fr>
2016-04-01 12:06:22 +0200

    mise en place travis

--- projet/src/main/java/fr/uga/erods/ClientItf.java
+++ projet/src/main/java/fr/uga/erods/ClientItf.java
@@ -1,8 +1,13 @@
 package fr.uga.erods;
 
-public interface ClientItf {
+public interface ClientItf <T>{
 	
 	public void connect(Serveur s);
 	public void disconnect();
+	public void laddKey();
+	public void raddKey();
+	public void rmKey();
+	public <T> getKey();
+	public void setKey();
 
 }
